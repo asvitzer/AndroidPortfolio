ec70bd3836694730ac226eba85beca79c460e6b0
Alvin Svitzer <alsvitzer@gmail.com>
2016-12-29 19:54:33 -0600

    Converting from JSONObject to JSONArray

--- FlixBook/app/src/main/java/com/alvinsvitzer/flixbook/MovieGridActivity.java
+++ FlixBook/app/src/main/java/com/alvinsvitzer/flixbook/MovieGridActivity.java
@@ -2,23 +2,32 @@ package com.alvinsvitzer.flixbook;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.alvinsvitzer.flixbook.model.Movie;
+import com.alvinsvitzer.flixbook.model.MovieDBJSONUtils;
 import com.alvinsvitzer.flixbook.utilities.MovieDBUtils;
 import com.alvinsvitzer.flixbook.utilities.VolleyNetworkSingleton;
 import com.android.volley.Request;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonArrayRequest;
 import com.android.volley.toolbox.JsonObjectRequest;
 
+import org.json.JSONArray;
+import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.net.URL;
 
 public class MovieGridActivity extends AppCompatActivity {
 
+    private static final String TAG = MovieGridActivity.class.getSimpleName();
     private TextView mTextView;
     private String movieDBApiKey;
+    private Movie[] mMovieObjects;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,6 +35,8 @@ public class MovieGridActivity extends AppCompatActivity {
         setContentView(R.layout.activity_movie_grid);
 
         mTextView = (TextView) findViewById(R.id.text_view_query_result);
+
+        /** Set movieDBApiKey below to your API key**/
         movieDBApiKey = getString(R.string.the_movie_db_auth_key);
 
         grabDefaultMovie();
@@ -37,24 +48,36 @@ public class MovieGridActivity extends AppCompatActivity {
 
         URL requestDefaultMovieURL = MovieDBUtils.buildDefaultUrl(movieDBApiKey);
 
-
-        JsonObjectRequest jsObjRequest = new JsonObjectRequest
-                (Request.Method.GET, requestDefaultMovieURL.toString(), null, new Response.Listener<JSONObject>() {
+        JsonArrayRequest jsArrayRequest = new JsonArrayRequest
+                (Request.Method.GET, requestDefaultMovieURL.toString(), null, new Response.Listener<JSONArray>() {
 
                     @Override
-                    public void onResponse(JSONObject response) {
-                        mTextView.setText("Response: " + response.toString());
+                    public void onResponse(JSONArray response) {
+
+                        try {
+                            mMovieObjects = MovieDBJSONUtils.getMovieDataFromJSONObject(MovieGridActivity.this, response);
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+
                     }
+
+
                 }, new Response.ErrorListener() {
 
                     @Override
                     public void onErrorResponse(VolleyError error) {
                         // TODO Auto-generated method stub
 
+                        Log.e(TAG, "onErrorResponse: ", error);
+
+                        Toast.makeText(MovieGridActivity.this, "Unable to retrieve movie data.", Toast.LENGTH_SHORT)
+                                .show();
+
                     }
                 });
 
-        VolleyNetworkSingleton.getInstance(this).addToRequestQueue(jsObjRequest);
+        VolleyNetworkSingleton.getInstance(this).addToRequestQueue(jsArrayRequest);
 
     }
 }
---
+++ FlixBook/app/src/main/java/com/alvinsvitzer/flixbook/model/Movie.java
@@ -0,0 +1,56 @@
+package com.alvinsvitzer.flixbook.model;
+
+/**
+ * Created by Alvin on 12/29/16. Model Movie object.
+ */
+
+public class Movie {
+
+    private String movieTitle;
+    private String moviePoster;
+    private String plotSynopsis;
+    private Double userRating;
+    private String releaseDate;
+
+    public String getMovieTitle() {
+        return movieTitle;
+    }
+
+    public void setMovieTitle(String movieTitle) {
+        this.movieTitle = movieTitle;
+    }
+
+    public String getMoviePoster() {
+        return moviePoster;
+    }
+
+    public void setMoviePoster(String moviePoster) {
+        this.moviePoster = moviePoster;
+    }
+
+    public String getPlotSynopsis() {
+        return plotSynopsis;
+    }
+
+    public void setPlotSynopsis(String plotSynopsis) {
+        this.plotSynopsis = plotSynopsis;
+    }
+
+    public Double getUserRating() {
+        return userRating;
+    }
+
+    public void setUserRating(Double userRating) {
+        this.userRating = userRating;
+    }
+
+    public String getReleaseDate() {
+        return releaseDate;
+    }
+
+    public void setReleaseDate(String releaseDate) {
+        this.releaseDate = releaseDate;
+    }
+
+
+}
