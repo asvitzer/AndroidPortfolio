595fdc4134f1350bffdf4b0b3169e423d4c99f23
Alvin Svitzer <alsvitzer@gmail.com>
2017-02-21 23:02:51 +0100

    Changing Injection to use Build Variants

--- FlixBook/app/src/prod/java/com/alvinsvitzer/flixbook/Injection.java
+++ FlixBook/app/src/prod/java/com/alvinsvitzer/flixbook/Injection.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright (C) 2015 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.alvinsvitzer.flixbook;
 
 import android.content.Context;
@@ -9,6 +25,10 @@ import com.alvinsvitzer.flixbook.data.remote.MovieRemoteDataStore;
 
 import static com.google.common.base.Preconditions.checkNotNull;
 
+/**
+ * Will try to use this setup to inject mock implementations of the data store to be able to
+ * more effectively using Espresso UI testing since I can override the mock version to use static data.
+ */
 public class Injection {
 
     public static AppRepository provideMovieDataStoreRepository(@NonNull Context context) {
